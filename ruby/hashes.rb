117730c85992a7848fa636093f029335ae5a7712
Danny Kalemba <Kalemba@gmail.com>
2016-09-23 00:28:10 -0500

    Add user input to hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,13 +6,34 @@
 #-change hash value
 
 
-client_information = {name: "", age: 0, childern: 0}
+client_information = {}
 
-def update_hash
+# {name: "", age: 0, childern: 0, decor_theme: ""}
 
-
+def update_hash(update_key)
+  key
 end
 
-def ui
-
+puts "Enter client name:"
+  client_information[:name] = gets.chomp
+puts "Enter client age:"
+  client_information[:age] = gets.chomp.to_i
+puts "Is the client married?(y/n)"
+  client_information[:married] = gets.chomp
+puts "How many children does the client have?"
+  client_information[:children] = gets.chomp.to_i
+puts "What is the decor_theme?"
+  client_information[:decor_theme] = gets.chomp
+if client_information[:married] == "y"
+  client_information[:married] = true
+else
+  client_information[:married] = false
 end
+puts client_information
+
+puts "Enter in the client information you would like to update:"
+  if gets.chomp == "none"
+    puts client_information  
+  else
+    update_hash(gets.chomp)
+  end
