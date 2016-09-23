b2b45742b1536ff90a5460b137ab68acf54dd6f7
Danny Kalemba <Kalemba@gmail.com>
2016-09-22 19:38:05 -0500

    create hash and pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,18 @@
+#-Hashes
+#-create hash 
+#-user input info
+#-print info hash to screen
+#-ask user if they want to update info
+#-change hash value
+
+
+client_information = {name: "", age: 0, childern: 0}
+
+def update_hash
+
+
+end
+
+def ui
+
+end
