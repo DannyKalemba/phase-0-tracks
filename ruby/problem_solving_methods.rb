12e4f00f64187c822daa6b0b3e1dcafab12169ea
Danny Kalemba <Kalemba@gmail.com>
2016-09-23 19:12:13 -0500

    Create Fib method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -13,3 +13,21 @@ def search_array(arr, number)
 end
 
 search_array([1,2,3,4,6,7,835,45], 85)
+
+
+def fib(number)
+  index = 0
+  arr= [0,1]
+  until index >= number-2
+    arr << arr[index] + arr[index+1]
+    index += 1
+  end
+  return arr
+end
+
+p fib(6)
+p fib(100)[-1] == 218922995834555169026
+
+def method_name
+  
+end
