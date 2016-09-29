355a270112f7148b6e9766bf39e0d6c7a3e57ebe
Danny Kalemba <Kalemba@gmail.com>
2016-09-29 00:34:10 -0500

    Create tests for rspec file

---
+++ ruby/game/game.rb
@@ -0,0 +1,11 @@
+class Game
+  attr_reader :guess_count
+  attr_accessor :
+
+  def initialize
+    @
+    @guess_count = 0
+  end
+
+
+end
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,8 +1,32 @@
+# -Our word guessing game should ...
+# -limit number of guesses based on length of word
+# -return if letter guessed is in word
+# -reveal letter
+# -input word
+# -return taunting message
+# -return congratulatory message
+
 require_relative 'game'
 
 describe Game do
-  let(:) {Game.new()}
+  let(:game) {Game.new}
 
-  it " "
+  it "return an array of chars for the word" do
+    expect(game.return_array('unicorn')).to eq ['u','n','i','c','o','r','n']
+  end
+
+  it "return number of guesses" do 
+    expect(game.return_guess('unicorn')).to eq 7
+  end
+
+  it "return true if letter guessed is in the word" do
+    expect(game.guess('u')).to eq true
+  end
+
+  it "return true if letter was already guessed" do
+    expect(guessed_letter('u')).to eq true
+  end
+
+  
 
 end
