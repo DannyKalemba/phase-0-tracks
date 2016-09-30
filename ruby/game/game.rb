cac126457887d5a3adbd19ad8ca44c7b9560b953
Danny Kalemba <Kalemba@gmail.com>
2016-09-29 22:27:49 -0500

    Create tests in rspec and methods in class so they pass

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,11 +1,35 @@
 class Game
   attr_reader :guess_count
-  attr_accessor :
+  attr_accessor :word
 
-  def initialize
-    @
-    @guess_count = 0
+  def initialize(arr)
+    @word = arr.chars
+    @guess_count = 0 
   end
 
+  def check_letter(letter)
+    @guess_count += 1
+    if @word.include?(letter)
+      true
+    else
+      false
+    end    
+  end
 
+  def repeated_guess(letter, arr)
+    if arr.include?(letter)
+      @guess_count -= 1
+      true
+    else
+      false
+    end
+  end
+
+  def return_total_guess(arr)
+    arr.length - 2
+  end
+
+  def method_name
+    
+  end
 end
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -9,22 +9,22 @@
 require_relative 'game'
 
 describe Game do
-  let(:game) {Game.new}
+  let(:game) {Game.new('unicorn')}
 
-  it "return an array of chars for the word" do
-    expect(game.return_array('unicorn')).to eq ['u','n','i','c','o','r','n']
+  it "return an array of chars for the word on initialization" do
+    expect(game.word).to eq ['u','n','i','c','o','r','n']
   end
 
-  it "return number of guesses" do 
-    expect(game.return_guess('unicorn')).to eq 7
+  it "return total number of guesses" do 
+    expect(game.return_total_guess('unicorn')).to eq 5
   end
 
   it "return true if letter guessed is in the word" do
-    expect(game.guess('u')).to eq true
+    expect(game.check_letter('u')).to eq true
   end
 
   it "return true if letter was already guessed" do
-    expect(guessed_letter('u')).to eq true
+    expect(game.repeated_guess('u',['u','_'])).to eq true
   end
 
   
