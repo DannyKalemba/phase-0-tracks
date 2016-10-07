9702781b6869c12dcca410c658d4894abf7b5872
Danny Kalemba <Kalemba@gmail.com>
2016-10-06 23:01:14 -0500

    Add function to find if keys and values match in an object

--- js/algos.js
+++ js/algos.js
@@ -2,7 +2,7 @@
 //if item is longer than string replace string
 //return the string
 
-function longest(arr){
+function Longest(arr){
   var longestString = ""
   for (i = 0; i < arr.length; i++){
     if (arr[i].length > longestString.length){
@@ -12,14 +12,32 @@ function longest(arr){
   return longestString;
 }
 
-function
+//Go through first object check if key matches second object 
+//Go through second object and check if keys match
+function Match(object, objectTwo){
+  for (var key in object){
+    for(var k in objectTwo){
+      if (key === k && object[key] === objectTwo[k] ){
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
 
 
-array = ["one", "whoa", "times", "two", "july"];
-arrayTwo =["holy smokes", "this", "is", "a", "test"];
-arrayThree = ["wow", "hmm", "this works"]
-console.log(longest(array));
-console.log(longest(arrayTwo));
-console.log(longest(arrayThree));
+var array = ["one", "whoa", "times", "two", "july"];
+var arrayTwo =["holy smokes", "this", "is", "a", "test"];
+var arrayThree = ["wow", "hmm", "this works"]
+console.log(Longest(array));
+console.log(Longest(arrayTwo));
+console.log(Longest(arrayThree));
 
+var hash = {age: 54, name: "Steven"};
+var hashTwo = {name: "Tamir", age: 54};
+var test = {age: 40, name: "Bobby", sex: "male"};
+var test2 = {name: "Bob", age: 42};
+
+console.log(Match(hash, hashTwo));
+console.log(Match(test, test2));
