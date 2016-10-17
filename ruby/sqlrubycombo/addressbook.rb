568d0138f09cce6e2108817325c06692ddbcf731
Danny Kalemba <Kalemba@gmail.com>
2016-10-17 09:26:38 -0500

    Create user interface

--- ruby/sqlrubycombo/addressbook.rb
+++ ruby/sqlrubycombo/addressbook.rb
@@ -18,7 +18,17 @@ insert_item_cmd = <<-SQL
   INSERT INTO address_book (name, address, phone) VALUES (?, ?, ?)
 SQL
 
-def insert_into(name address, phone)
+delete_item_cmd = <<-SQL
+  DELETE FROM address_book (name) WHERE name = ?
+SQL
+
+
+def insert_into(db, name, address, phone)
+  db.execute(insert_item_cmd, name, address, phone)
+end
+
+def delete_from(db, name)
+  db.execute(delete_item_cmd, name)
   
 end
 
@@ -27,3 +37,16 @@ db.execute(create_table_cmd)
 
 #user interface
 puts "Welcome to your address book"
+puts "Enter a name you would like to add:"
+name = gets.chomp
+puts "Enter in their address:"
+address = gets.chomp
+puts "Enter in their phone number:"
+phone = gets.chomp
+
+insert_into(db, name, address, phone)
+puts "Enter a name you would like remove from your address book:"
+name = gets.chomp
+delete_from(db, name)
+puts "Here is your address book"
+db.execute("SELECT * FROM address_book")
