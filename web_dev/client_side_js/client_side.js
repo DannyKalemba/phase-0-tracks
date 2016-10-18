0c4bae31a9d9518c3ca9fd70c3464d3cc866a1e9
Danny Kalemba <Kalemba@gmail.com>
2016-10-18 12:38:53 -0500

    formatting

--- web_dev/client_side_js/client_side.js
+++ web_dev/client_side_js/client_side.js
@@ -4,10 +4,8 @@ var sidebar = document.getElementById('cast');
 
 sidebar.style.border = "5px solid orange";
 
-
 function hide_director(event){
   document.getElementsByClassName('director')[0].hidden = true;
 }
-
 
 addEventListener("keydown", hide_director);
